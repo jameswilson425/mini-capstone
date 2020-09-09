054f88e06acd222403d9bf8bdfcdc1788fcb459e
jimmy <jimewilson87@gmail.com>
2020-09-09 14:35:01 -0500

    updates json variables

--- product_http.rb
+++ product_http.rb
@@ -2,7 +2,7 @@ require "http"
 
 system "clear"
 
-puts "Welcome to my store. "
+puts "Welcome to my store. Here are all of my products:"
 
 response = HTTP.get("http://localhost:3000/api/all_products.json")
 
