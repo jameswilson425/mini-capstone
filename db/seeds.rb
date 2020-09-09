955116562f318f4ed2978c9197429636c3afaa50
jimmy <jimewilson87@gmail.com>
2020-09-09 12:05:31 -0500

    adds products seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Product.create(name: "Record Player", price: 100, image_path: "", description: "Vinyl record player with three speed settings. Includes 7in. record adapter.")
+
+Product.create(name: "Stereo Receiver", price: 150, image_path: "", description: "200 watt stereo receiver. Includes AM/FM radio adapter.")
+
+Product.create(name: "Speakers", price: 75, image_path: "", description: "Two 200-watt stereo speakers.")
