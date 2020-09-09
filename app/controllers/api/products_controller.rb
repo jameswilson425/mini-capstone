1254466086fb8e07cbfc31ca447dead88ef6f70e
jimmy <jimewilson87@gmail.com>
2020-09-09 12:51:36 -0500

    adds individual product routes

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -3,4 +3,19 @@ class Api::ProductsController < ApplicationController
     @products = Product.all
     render "all_products.json.jb"
   end
+
+  def record_player
+    @turntable = Product.find_by(id: 1)
+    render "r_player.json.jb"
+  end
+
+  def receiver
+    @receiver = Product.find_by(id: 2)
+    render "stereo.json.jb"
+  end
+
+  def speakers
+    @speakers = Product.find_by(id: 3)
+    render "speakers.json.jb"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,13 @@ Rails.application.routes.draw do
   namespace :api do
     get "/all_products" => "products#all_products"
   end
+  namespace :api do
+    get "/r_player" => "products#record_player"
+  end
+  namespace :api do
+    get "/stereo" => "products#receiver"
+  end
+  namespace :api do
+    get "/speakers" => "products#speakers"
+  end
 end
